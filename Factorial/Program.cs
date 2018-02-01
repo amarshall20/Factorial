d336d633bfac29e0c77a888a246cf153e963f6b9
amarshall20 <33075034+amarshall20@users.noreply.github.com>
2018-02-01 16:31:21 -0700

    Initial commit

---
+++ Factorial/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select a number.");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(RecursiveFactorial(n));
+            Console.ReadLine();
+        }
+        static int LoopingFactorial(int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+        static int RecursiveFactorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * RecursiveFactorial(n - 1);
+        }
+    }
+}
